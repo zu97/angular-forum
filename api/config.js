06565cc7df0d69e4baa39d2e5330974532657a76
admin <admin@admin.com>
2022-03-11 18:04:34 +0600

    Setting up a database connection for api

---
+++ api/config.js
@@ -0,0 +1,9 @@
+const rootPath = __dirname;
+
+module.exports = {
+    rootPath,
+    mongoConfig: {
+        url: 'mongodb://localhost/hw87_zush',
+        options: { useNewUrlParser: true }
+    }
+};
--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('./config');
+
 const app = express();
 const port = 8000;
 
-app.listen(port, () =>  {
-    console.log(`Server started on ${port} port`);
-});
+app.use(cors());
+
+(async () => {
+    await mongoose.connect(config.mongoConfig.url, config.mongoConfig.options);
+
+    app.listen(port, () =>  {
+        console.log(`Server started on ${port} port`);
+    });
+
+    process.on('exit', () => {
+        mongoose.disconnect();
+    });
+})();
