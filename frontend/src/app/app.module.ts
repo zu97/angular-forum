2660bb6e98f2730b3d7eaea2bf17667290cbc66d
admin <admin@admin.com>
2022-03-12 10:23:32 +0600

    Creating a ui  loader component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,18 +8,21 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 import { LayoutComponent } from './ui/layout/layout.component';
 import { FileInputComponent } from './ui/file-input/file-input.component';
+import { LoaderComponent } from './ui/loader/loader.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageNotFoundComponent,
     LayoutComponent,
-    FileInputComponent
+    FileInputComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +33,8 @@ import { FileInputComponent } from './ui/file-input/file-input.component';
     MatToolbarModule,
     MatButtonModule,
     MatSidenavModule,
-    FormsModule
+    FormsModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
